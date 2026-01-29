f527503ca23dd2f3afcecc48a41cb51ede855cb3
ronaldosabino <106283721+ronaldosabino@users.noreply.github.com>
2026-01-29 19:28:52 -0300

    feat: update portfolio with new resume and enhance app layout
    - Updated resume details in PDF format with new contact information and professional experience.
    - Improved app layout by adding responsive design elements and enhancing the navigation menu.
    - Added carousel functionality for showcasing skills with images and descriptions.
    - Fixed minor CSS transitions for better visual effects.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
 
   download() {
     const link = document.createElement('a');
-    link.href = 'assets/curriculo_Ronaldo_Sabino.pdf';
+    link.href = 'assets/Curriculo_Ronaldo_Sabino.pdf';
     link.download = 'Curriculo_Ronaldo_Sabino.pdf';
     link.click();
   }
@@ -40,4 +40,32 @@ export class AppComponent {
     this.controlCarousel = true;
     console.log('control', this.controlCarousel);
   }
+
+  carouselItems: any[] = [
+    {
+      title: 'Angular',
+      description: 'Framework baseado em TypeScript',
+      image: 'assets/angular.png',
+    },
+    {
+      title: 'TypeScript',
+      description: 'Linguagem de programação',
+      image: 'assets/typescript.png',
+    },
+    {
+      title: 'Bootstrap',
+      description: 'Biblioteca de estilização',
+      image: 'assets/bootstrap.png',
+    },
+    {
+      title: 'GitLab',
+      description: 'Ferramenta de versionamento',
+      image: 'assets/gitlab.png',
+    },
+    {
+      title: 'K6 Grafana',
+      description: 'Ferramenta de testes de carga',
+      image: 'assets/k6.png',
+    },
+  ];
 }
